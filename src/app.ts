6725d4aa57db8471b6d2d575c9c52197051a3ddb
Arkadiusz Milewski <asmilewski@gmail.com>
2022-06-09 12:30:33 +0200

    active projects made draggable

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+// Drag and Drop interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+ }
+
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+
+}
+
+
 //Project Type
 enum ProjectStatus { Active, Finished }
 
@@ -140,9 +155,10 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract configure(): void; 
   abstract renderContent(): void;
 }
-// PojectItemClass
+// Poject Item Class
 
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable {
   private project: Project;
 
   get persons() {
@@ -161,8 +177,18 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent(); 
   }
 
-  configure() {
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    console.log(event)
+  }
 
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd')
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
   }
 
   renderContent()  {
